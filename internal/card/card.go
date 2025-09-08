2f4c38b4a4a0f604a1eb0de1e183e0bc0bd5c7ff
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-08 09:31:33 +0300

    create card backend logic

---
+++ internal/card/card.go
@@ -0,0 +1,64 @@
+// Package card for setting the card stripe integration logic.
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transactions struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
+
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	payIntent, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+	return payIntent, "", nil
+}
+
+func cardErrorMessage(code stripe.ErrorCode) string {
+	msg := ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your Card Was Declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your Card is Expired"
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect ZIP code"
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Incorrect CVC code"
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge your card"
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge your card"
+	default:
+		msg = "Your Card Was Declined"
+	}
+	return msg
+}
