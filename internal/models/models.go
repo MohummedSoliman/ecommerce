c01c9a3586f646d51112e7c8d556c51033b85422
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-09 10:33:22 +0300

    create models file that handle different connection to different DBs

---
+++ internal/models/models.go
@@ -0,0 +1,32 @@
+// Package models define the type of database connection values.
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type DBModel struct {
+	DB *sql.DB
+}
+
+// Models is wrapper for all models.
+type Models struct {
+	DB DBModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		DB: DBModel{DB: db},
+	}
+}
+
+type Widget struct {
+	ID             int       `json:"id"`
+	Name           string    `json:"name"`
+	Description    string    `json:"description"`
+	InventroyLevel int       `json:"inventory_level"`
+	Price          int       `json:"price"`
+	CreatedAt      time.Time `json:"-"`
+	UpdatedAt      time.Time `json:"-"`
+}
