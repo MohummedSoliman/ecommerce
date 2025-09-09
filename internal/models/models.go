125e413358a9fa9f3633cc53bb16d713074ee765
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-09 22:14:37 +0300

    create equivelant struct for DB tables

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,6 +2,7 @@
 package models
 
 import (
+	"context"
 	"database/sql"
 	"time"
 )
@@ -27,6 +28,75 @@ type Widget struct {
 	Description    string    `json:"description"`
 	InventroyLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
+	Image          string    `json:"image"`
 	CreatedAt      time.Time `json:"-"`
 	UpdatedAt      time.Time `json:"-"`
+}
+
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetID      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreatedAt     time.Time `json:"-"`
+	UpdatedAt     time.Time `json:"-"`
+}
+
+type Status struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+type TransactionStatus struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+type Transaction struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	Currency            string    `json:"currency"`
+	LastFour            string    `json:"last_four"`
+	BankReturnCode      string    `json:"bank_return_code"`
+	TransactionStatusID int       `json:"transaction_status_id"`
+	CreatedAt           time.Time `json:"-"`
+	UpdatedAt           time.Time `json:"-"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+func (m *DBModel) GetWedgit(id int) (Widget, error) {
+	ctx, cencel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cencel()
+
+	var widget Widget
+
+	query := `SELECT id, name FROM widgets WHERE id =$1`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(&widget.ID, &widget.Name)
+	if err != nil {
+		return widget, err
+	}
+
+	if err = row.Err(); err != nil {
+		return widget, err
+	}
+
+	return widget, nil
 }
