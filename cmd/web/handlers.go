56f8aef47e02cf4fe7935ec0dc9abd48a67803f9
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-09 15:01:31 +0300

    remove storing stripe key in every request

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,11 +7,7 @@ import (
 )
 
 func (app *application) VertualTerminal(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["publishable_key"] = app.config.stripe.key
-	err := app.renderTemplate(w, r, "terminal", &templateData{
-		StringMap: stringMap,
-	}, "stripe-js")
+	err := app.renderTemplate(w, r, "terminal", &templateData{}, "stripe-js")
 	if err != nil {
 		app.errorLog.Println(err)
 	}
